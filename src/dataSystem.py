738d71f6fbcf7527abb1f5de83b3b4d8df235ade
RikkHardi <48567195+RikkHardi@users.noreply.github.com>
2021-10-06 19:55:38 +0300

    Dokumenteerimine lisatud

--- src/dataSystem.py
+++ src/dataSystem.py
@@ -7,12 +7,15 @@ import pickle
 #MMM   MMM MMM  MMM MMM  MMM MMM  MMM MMM  MMM
 #See siin on Hannale
 #Hanna, sulle vajalikud käsud:
+# - Saadavat sõnastikku saad järgmiselt kasutada:
+# -- sõnastik[kuupäev(str)][sündmuse nimi(str)][andme index]
 # - timeplan.set(date, event, startTime, endTime, desc)
 # -- selle käsuga talletab sündmusi ja muid sellised
 # - timeplan.get()
 # -- selle käsuga võtad andmebaasist sündmusi
 # -- Tagastatud nimekirja saab kasutada järgmiselt (kuupäeva formaat on MM/DD/YY):
 # --- eventsList = timeplan.get()
+# --- events = events[date]
 # --- event = events[date][eventName]
 # --- eventStart = event[0]
 # --- eventEnd = event[1]
@@ -54,6 +57,11 @@ class timeplan:
             loadedData = pickle.load(timeplanData)
             return loadedData
 #See osa Kaalebile
+# käsud:
+# - notes.set(notes)
+# -- Salvestab parajasti kirjutatud asjad ära
+# - notes.get()
+# -- saad salvestatud märkmed
 class notes:
     def set(notes):
         with open('notes.pickle', 'wb') as notesData:
