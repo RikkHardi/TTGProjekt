c397f238187ad46e1b3ce540e9e261f208d36e21
Kaalebhumal <90179431+Kaalebhumal@users.noreply.github.com>
2021-10-15 16:05:09 +0300

    Update test_kaaleb.py

--- test_kaaleb.py
+++ test_kaaleb.py
@@ -1,1 +1,10 @@
-eeeeoioiii
+a=input("Sisestage siia mÃ¤rkmeid oma kalendrisse:")
+read = []
+a.append(read)
+fail = open("markmed.txt", encoding="UTF-8")
+
+for rida in read:
+  read.append(fail)
+fail.close()
+
+print(fail) 
